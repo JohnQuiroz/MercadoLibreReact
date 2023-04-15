57d0c3a7b4fb4fe1fa41ac4c6c905cf20a125162
yejota <yeisonjimenezids@gmail.com>
2023-04-15 17:03:16 -0500

    Implementación de la sección discover con next

--- mercado-libre-react/layouts/Layout.tsx
+++ mercado-libre-react/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import {Advantages} from '@components/Advantages';
 import {Banner} from '@components/Banner';
+import { Discover } from '@components/Discover';
 import {Offers} from '@components/Offers';
 import { Header } from 'components/Header';
 import React from 'react';
@@ -167,43 +168,7 @@ const Layout = () => {
                                 </div>
                             </div>
                         </section>
-                        <section className="discover-section">
-                            <div className="discover-title">
-                                <span>Descubre</span>
-                            </div>
-                            <div className="discover-description-container">
-                                <a href="#">
-                                    <div className="discover-description-content">
-                                        <div className="discover-description-content-text">
-                                            <span className="discover-description-content-text-1">RENUEVA TUS ESPACIOS</span>
-                                            <span>HOGAR Y MUEBLES</span>
-                                            <span>HASTA 50% OFF</span>
-                                            <div>
-                                                <button>Ver más</button>
-                                            </div>
-                                        </div>
-                                        <div className="discover-description-content-img">
-                                            <img src="media/descubre 1.png" alt="Descubre 1" />
-                                        </div>
-                                    </div>
-                                </a>
-                                <a href="#">
-                                    <div className="discover-description-content">
-                                        <div className="discover-description-content-text">
-                                            <span className="discover-description-content-text-1">SUSCRÍBETE AL NIVEL 6</span>
-                                            <span>POR SOLO</span>
-                                            <span>$17899 CADA MES</span>
-                                            <div>
-                                                <button>Suscribirme</button>
-                                            </div>
-                                        </div>
-                                        <div className="discover-description-content-img">
-                                            <img src="media/descubre 2.png" alt="Descubre 2" />
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                        </section>
+                        <Discover/>
                         <section className="stores-section">
                             <div>
                                 <div className="stores-title">
