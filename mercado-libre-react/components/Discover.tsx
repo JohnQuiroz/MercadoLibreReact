12574cf226c26878027ca5a6fd06ceb172455b91
John Quiroz <bayron.quiroz@udea.edu.co>
2023-04-18 21:33:32 -0500

    Se corrigen algunos errores en el Layout para incluir los componetes faltantes

--- mercado-libre-react/components/Discover.tsx
+++ mercado-libre-react/components/Discover.tsx
@@ -1,45 +1,46 @@
+import Link from 'next/link';
 import React from 'react'
 
 const Discover = () => {
-  return (
-    <section className="discover-section">
-                            <div className="discover-title">
-                                <span>Descubre</span>
+    return (
+        <section className="discover-section">
+            <div className="discover-title">
+                <span>Descubre</span>
+            </div>
+            <div className="discover-description-container">
+                <Link href="/">
+                    <div className="discover-description-content">
+                        <div className="discover-description-content-text">
+                            <span className="discover-description-content-text-1">RENUEVA TUS ESPACIOS</span>
+                            <span>HOGAR Y MUEBLES</span>
+                            <span>HASTA 50% OFF</span>
+                            <div>
+                                <button>Ver más</button>
                             </div>
-                            <div className="discover-description-container">
-                                <a href="#">
-                                    <div className="discover-description-content">
-                                        <div className="discover-description-content-text">
-                                            <span className="discover-description-content-text-1">RENUEVA TUS ESPACIOS</span>
-                                            <span>HOGAR Y MUEBLES</span>
-                                            <span>HASTA 50% OFF</span>
-                                            <div>
-                                                <button>Ver más</button>
-                                            </div>
-                                        </div>
-                                        <div className="discover-description-content-img">
-                                            <img src="media/descubre 1.png" alt="Descubre 1" />
-                                        </div>
-                                    </div>
-                                </a>
-                                <a href="#">
-                                    <div className="discover-description-content">
-                                        <div className="discover-description-content-text">
-                                            <span className="discover-description-content-text-1">SUSCRÍBETE AL NIVEL 6</span>
-                                            <span>POR SOLO</span>
-                                            <span>$17899 CADA MES</span>
-                                            <div>
-                                                <button>Suscribirme</button>
-                                            </div>
-                                        </div>
-                                        <div className="discover-description-content-img">
-                                            <img src="media/descubre 2.png" alt="Descubre 2" />
-                                        </div>
-                                    </div>
-                                </a>
+                        </div>
+                        <div className="discover-description-content-img">
+                            <img src="media/descubre 1.png" alt="Descubre 1" />
+                        </div>
+                    </div>
+                </Link>
+                <Link href="/">
+                    <div className="discover-description-content">
+                        <div className="discover-description-content-text">
+                            <span className="discover-description-content-text-1">SUSCRÍBETE AL NIVEL 6</span>
+                            <span>POR SOLO</span>
+                            <span>$17899 CADA MES</span>
+                            <div>
+                                <button>Suscribirme</button>
                             </div>
-                        </section>
-  );
+                        </div>
+                        <div className="discover-description-content-img">
+                            <img src="media/descubre 2.png" alt="Descubre 2" />
+                        </div>
+                    </div>
+                </Link>
+            </div>
+        </section>
+    );
 };
 
-export {Discover};
+export { Discover };
