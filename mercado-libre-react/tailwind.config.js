7f7433fbc3a80d0ba8ee2e542e1fcf604b084692
John Quiroz <bayron.quiroz@udea.edu.co>
2023-04-23 19:17:41 -0500

    Se implementan los estilos de TailwindCSS en la sección Stores

--- mercado-libre-react/tailwind.config.js
+++ mercado-libre-react/tailwind.config.js
@@ -30,16 +30,21 @@ module.exports = {
         'black': '#000000',
       },
       boxShadow: {
+        '15': '0px 2px 15px 0px rgba(0, 0, 0, 0.184)',
         '20': '0 1px 2px 0 rgb(0, 0, 0, 20%)',
       },
-
       backgroundImage: {
         'beneficios1': "url('/media/beneficios 1.png')",
         'beneficios2': "url('/media/beneficios 2.png')",
         'beneficios3': "url('/media/beneficios 3.png')",
-
+        'mejores1': "url('/media/mejores 1.png')",
+        'mejores2': "url('/media/mejores 2.png')",
+        'mejores3': "url('/media/mejores 3.png')",
+        'mejores4': "url('/media/mejores 4.png')",
       },
-
+      backgroundSize: {
+        '100': '100%',
+      },
       fontSize: {
         '11px': '.688rem',
         '13px': '.813rem',
