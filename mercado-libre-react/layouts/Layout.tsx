6158485b36adbca120edf140cdc136c83aa7e413
John Quiroz <bayron.quiroz@udea.edu.co>
2023-04-23 23:07:56 -0500

    Se implementa el Footer con NextJS

--- mercado-libre-react/layouts/Layout.tsx
+++ mercado-libre-react/layouts/Layout.tsx
@@ -10,6 +10,7 @@ import { Suscribirse } from '@components/Suscribirse';
 import Collections from '@components/Collections';
 import Categories from '@components/Categories';
 import Information from '@components/Information';
+import Footer from '@components/Footer';
 
 const Layout = () => {
     return (
@@ -33,32 +34,7 @@ const Layout = () => {
                     </div>
                 </div>
             </main>
-            <footer>
-                <div className="copyright">
-                    <div className="copyright-links">
-                        <ul className="interest-links">
-                            <li><a href="#">Trabaja con nosotros</a></li>
-                            <li><a href="#">Términos y condiciones</a></li>
-                            <li><a href="#">Cómo cuidamos tu privacidad</a></li>
-                            <li><a href="#">Accesibilidad</a></li>
-                            <li><a href="#">Ayuda / PQR</a></li>
-                            <li><a href="#">www.sic.gov.co</a></li>
-                        </ul>
-                        <span>Copyright © 1999-2023 MercadoLibre Colombia LTDA.</span>
-                    </div>
-                    <div className="copyright-address">
-                        <span>Carrera 17 Numero 93 - 09 Piso 3, Bogotá D.C., Colombia</span>
-                    </div>
-                </div>
-                <div className="endorse">
-                    <div>
-                        <img src="media/logo industria y comercio.png" alt="Industria y Comercio" />
-                    </div>
-                    <div>
-                        <img src="media/logo pare compare.png" alt="Pare y Compare" />
-                    </div>
-                </div>
-            </footer>
+            <Footer />
         </div>
     );
 };
